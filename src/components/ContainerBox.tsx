7738dab9774108df5fdbc63003a8fa90bf6a749f
Martin Jelenak <studiolang@gmail.com>
2024-07-15 10:27:04 +0200

    container box

--- src/components/ContainerBox.tsx
+++ src/components/ContainerBox.tsx
@@ -1,13 +1,20 @@
 import React from "react";
+import { cn } from "../utils/cn";
 
 interface ContainerBoxProps {
   children: React.ReactNode;
+  className?: string;
 }
 
-export default function ContainerBox({ children }: ContainerBoxProps) {
-  return (
-    <div className="bg-gray-300 ring-1 ring-opacity-5 ring-black p-4 w-auto dark:bg-slate-">
-      {children}
-    </div>
-  );
+export default function ContainerBox({
+  children,
+  className,
+}: ContainerBoxProps) {
+  const base = "p-4 ring-1 ring-opacity-5";
+  const light = "bg-gray-300 ring-black ";
+  const dark = "dark:bg-slate-900 dark:ring-slate-700";
+
+  const combinedStyles = cn(base, light, dark, className);
+
+  return <div className={combinedStyles}>{children}</div>;
 }
