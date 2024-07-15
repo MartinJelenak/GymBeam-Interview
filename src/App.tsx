c0d49a534fd5f9c37da4cc717af50008dcb2e87c
Martin Jelenak <studiolang@gmail.com>
2024-07-15 10:55:54 +0200

    header wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import DarkModeToggle from "./components/DarkModeToggle";
 import ContainerBox from "./components/ContainerBox";
+import ButtonAction from "./components/ButtonAction";
+import Header from "./components/Header";
 
 function App() {
   return (
@@ -9,8 +11,11 @@ function App() {
         bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(140,140,150,1),rgba(255,255,255,0))]
         dark:bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"
     >
-      <DarkModeToggle />
-      <ContainerBox>test</ContainerBox>
+      <Header />
+
+      <ContainerBox>
+        <ButtonAction variant="primary">Click me!</ButtonAction>
+      </ContainerBox>
       <div>Toto je obsah, ktorý mení farbu pozadia podľa režimu.</div>
     </div>
   );
