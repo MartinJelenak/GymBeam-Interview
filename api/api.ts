81aaad8b8c41053e71ac7f0ab5b820a95240aa7b
Martin Jelenak <studiolang@gmail.com>
2024-07-16 21:46:28 +0200

    type, intstall tanstack query

---
+++ api/api.ts
@@ -0,0 +1,54 @@
+import axios from 'axios';
+
+const API_BASE_URL = 'https://6602e5e39d7276a75554873a.mockapi.io/apiamcef/v1';
+
+export const updateTodoCompleted = async ({ todoListId, todoId, completed }: { todoListId: string, todoId: string, completed: boolean }): Promise<any> => {
+    const url = `${API_BASE_URL}/todolists/${todoListId}/todos/${todoId}`;
+    const response = await axios.put(url, {
+        completed,
+    });
+    return response.data;
+};
+
+export const createToDo = async ({ todoListId, title, description, deadline }: { todoListId: string, title: string, description: string, deadline: Date }): Promise<any> => {
+    const response = await axios.post(`${API_BASE_URL}/todolists/${todoListId}/todos`, {
+        title,
+        description,
+        deadline,
+        completed: false,
+    });
+    return response.data;
+}
+
+export const deleteToDo = async ({ todoListId, todoId }: { todoListId: string, todoId: string }): Promise<any> => {
+    const response = await axios.delete(`${API_BASE_URL}/todolists/${todoListId}/todos/${todoId}`);
+    return response.data;
+}
+
+
+
+
+
+export const fetchToDoLists = async () => {
+    const response = await axios.get(`${API_BASE_URL}/todolists`);
+    return response.data;
+}
+
+export const createToDoList = async ({ name }: { name: string }) => {
+    const response = await axios.post(`${API_BASE_URL}/todolists`, {
+        name,
+    });
+    return response.data;
+}
+
+export const deleteToDoList = async (id: string) => {
+    const response = await axios.delete(`${API_BASE_URL}/todolists/${id}`);
+    return response.data;
+}
+
+export const updateToDoList = async (id: string, name: string) => {
+    const response = await axios.put(`${API_BASE_URL}/todolists/${id}`, {
+        name,
+    });
+    return response.data;
+}
---
+++ types/index.ts
@@ -0,0 +1,23 @@
+export interface ToDoItemType {
+  id: string;
+  title: string;
+  description: string;
+  completed: boolean;
+  todolistId: string;
+}
+
+export interface ToDoListType {
+  id: string;
+  name: string;
+  todos: ToDoItemType[];
+}
+
+export interface ToDoListContainerProps {
+  todos: ToDoListType[];
+}
+
+export enum TodoFilter {
+  All = "all",
+  Active = "active",
+  Completed = "completed",
+}
